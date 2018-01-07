a5a1bdfeeded55ea2973afa031b4bf94032b560b
mkawasaki <mkawasaki@sbihg.co.jp>
2018-01-08 00:27:36 +0900

    add anycable

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require "action_cable/engine"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -14,5 +15,10 @@ module AnycableSample
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_cable.disable_request_forgery_protection = true
+    config.action_cable.url = Nenv.cable_url
+    config.action_cable.mount_path = Nenv.cable_url? ? nil : "/cable"
+
+
   end
 end
